fc2d4039dcd413260c887871a8b73056a51ee6bc
Brian Murphy-Dye <brian@murphydye.com>
2017-04-16 08:43:57 -0600

    Handle proxy request with missing content-length.

--- src/clj/rampart/services/http_handlers.clj
+++ src/clj/rampart/services/http_handlers.clj
@@ -46,87 +46,31 @@
 
 (defn proxy-request
   [from-path to-base-uri request & [http-opts]]
-  (println "\noriginal request:")
-  (clojure.pprint/pprint request)
-  (clojure.pprint/pprint http-opts)
+  ;; (println "\noriginal request:")
+  ;; (clojure.pprint/pprint request)
+  ;; (clojure.pprint/pprint http-opts)
   (let [rmt-full   (URI. (str to-base-uri "/"))
         rmt-path   (URI. (.getScheme    rmt-full)
                          (.getAuthority rmt-full)
                          (.getPath      rmt-full) nil nil)
         lcl-path   (URI. (subs (:uri request) (.length from-path)))
         to-uri (.resolve rmt-path lcl-path) ]
-    (println "to-uri:" to-uri)
-    (let [new-request
+    (let [has-content-length? (get-in request [:headers "content-length"])
+          new-request
           (merge {:method (:request-method request)
                   :url (str to-uri "?" (:query-string request))
                   :headers (dissoc (:headers request) "host" "content-length")
                   :body (if-let [len (get-in request [:headers "content-length"])]
-                          (slurp-binary (:body request) (Integer/parseInt len)))
+                          (slurp-binary (:body request) (Integer/parseInt len))
+                          (json/generate-string (:body request)))
                   ;; :body (json/generate-string (:body request))
-                  :body-params (:body-params request)
+                  ;; :body-params (:body-params request)
                   ;; :body "abc"
                   :follow-redirects true
                   :throw-exceptions false
-                  :as :stream} http-opts)
+                  :as (if has-content-length? :stream :json)} http-opts)
           ]
-      (println "\nnew request:")
-      (prn new-request)
-      (println "\n")
+      ;; (println "\nnew request:")
+      ;; (prn new-request)
+      ;; (println "\n")
       (client/request new-request))))
-;; (let [response (proxy-request "/" "https://www.summit.com/" ; "https://www.google.com/"
-;;                               {:uri "/"
-;;                                :request-method :get
-;;                                :query-string ""})]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
-
-;; (let [response (proxy-request "/" "https://www.summit.com/" ; "https://www.google.com/"
-;;                               {:uri "/api/v2/projects/3/releases"
-;;                                :request-method :get
-;;                                ;; :query-string ""
-;;                                })]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
-;; (let [response (proxy-request "/" "http://10.9.0.124:3005/"
-;;                               {:uri "/api/v2/projects/3/releases"
-;;                                :request-method :get
-;;                                ;; :query-string ""
-;;                                })]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
-
-;; (let [response (proxy-request "/" "http://10.9.0.124:3005/"
-;;                               {:uri "/api/v2/releases"
-;;                                :body {:attributes {:name "test 1"}
-;;                                       :relationships {:project {:data {:id 3}}
-;;                                                       :requestor {:data {:id 123}}}}
-;;                                :request-method :post
-;;                                ;; :query-string ""
-;;                                })]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
-;; (let [response (proxy-request "/" "http://10.9.0.124:3005/"
-;;                               {:uri "/api/v2/release-line-items"
-;;                                :body {:attributes {:quantity 3}
-;;                                       :relationships {:release {:data {:id "Release350657409571111"}}
-;;                                                       :project-line-item {:data {:id "1234"}}}}
-;;                                :request-method :post
-;;                                ;; :query-string ""
-;;                                })]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
-
-;; (let [response (proxy-request "/" "https://www.summit.com/" ; "https://www.google.com/"
-;;                               {:uri "/api/v2/projects/3/releases"
-;;                                :body {:name "test 1"}
-;;                                :request-method :post
-;;                                ;; :query-string ""
-;;                                })]
-;;   (clojure.pprint/pprint response)
-;;   (println "\n\nbody:")
-;;   (println (slurp (:body response))))
